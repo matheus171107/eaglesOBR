82a76873ccbca2ad6fa8c966fb34a050fad3dcb3
Pedro Campanari <pedrocampanari09@hotmail.com>
2023-11-28 10:20:37 -0300

    Library 'EaglesMotors' - V1.0

---
+++ eaglesMotors/eaglesMotors.ino
@@ -0,0 +1,45 @@
+class EaglesMotors {
+  int pinForward, pinBackward, pinVelocity;
+  public:
+    EaglesMotors(int pfw, int pbw, int pvt){
+      pinForward = pfw;
+      pinBackward = pbw;
+      pinVelocity = pvt;
+
+      pinMode(pfw, OUTPUT);
+      pinMode(pbw, OUTPUT);
+      pinMode(pvt, OUTPUT);
+
+    }
+    void setMoviment(char sense, int velocity){
+      if (sense == "F") {
+        digitalWrite(pinForward, HIGH);
+        digitalWrite(pinBackward, LOW);
+        analogWrite(pinVelocity, velocity);
+      }
+      if (sense == "B") {
+        digitalWrite(pinForward, LOW);
+        digitalWrite(pinBackward, HIGH);
+        analogWrite(pinVelocity, velocity);
+      }
+      if (sense == "R") {
+        digitalWrite(pinForward, HIGH);
+        digitalWrite(pinBackward, LOW);
+        analogWrite(pinVelocity, velocity);
+      }
+    }
+};
+
+EaglesMotors MOTOR_NAME_1(6, 7, 5);
+EaglesMotors MOTOR_NAME_2(4, 3, 10);
+
+
+
+
+void setup(){
+
+}
+void loop(){
+  MOTOR_NAME_1.setMoviment("B", 200);
+  MOTOR_NAME_2.setMoviment("F", 200);
+}
